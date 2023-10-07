06aa10013831cc0993080a243e1b13e43f315678
heuderdev <heuderdev@gmail.com>
2023-10-07 11:23:09 -0300

    projeto base

---
+++ src/main/java/com/javawebserviceengine/javawebserviceengine/JavawebserviceengineApplication.java
@@ -0,0 +1,13 @@
+package com.javawebserviceengine.javawebserviceengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavawebserviceengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavawebserviceengineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javawebserviceengine/javawebserviceengine/JavawebserviceengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javawebserviceengine.javawebserviceengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavawebserviceengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
